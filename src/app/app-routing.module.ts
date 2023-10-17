f1f746e3316de20c538565b82ce677031b1c0eec
Pablo Bogado <pablo.bogado@cavallaro.com.py>
2023-10-16 21:11:10 -0300

    Diseño de backend Sistema de tareas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { BoardsComponent } from './pages/boards/boards.component';
+import { DetboardComponent } from './pages/detboard/detboard.component';
+
+const routes: Routes = [
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path: 'boards',
+    component: BoardsComponent
+  },
+  {
+    path: 'detboards',
+    component: DetboardComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
