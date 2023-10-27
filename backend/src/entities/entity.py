66107b64f65b18c00c1ee6a5214938dfec669b06
David Gómez Bernal <dagobe2811@fpuna.edu.py>
2023-10-27 19:25:10 -0300

    Se modifica componente login y se sube codigo parcial del backend del login

---
+++ backend/src/entities/entity.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from sqlalchemy import create_engine, Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+db_url = 'localhost:5432'
+db_name = 'sistema_tareas'
+db_user = 'postgres'
+db_password = 'lcik'
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+Session = sessionmaker(bind=engine)
+
+
+Base = declarative_base()
+
+
